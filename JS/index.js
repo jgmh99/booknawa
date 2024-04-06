012747ab239f8d16121af7fe6ee1ba7cd0f3045a
Jegal MinHyuk <104841568+jgmh99@users.noreply.github.com>
2024-04-06 15:40:34 +0900

    .

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+// import { detailCategory } from './commonJs/detailCategory.mjs';
 document.addEventListener('DOMContentLoaded', function() {
     const detailCategory = [
             "Adventure Fiction",
@@ -25,8 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
             "Satire",
             "Science Fiction",
     ];
-
-
 
     /** 쿠키생성하고 저장하는 wwwwww */
     function setCookie(name, value, days) {
@@ -279,43 +278,5 @@ document.addEventListener('DOMContentLoaded', function() {
             .catch(error => console.error('Error:', error));
     }
     fetchPublishedDateRecentBooks();
-    
-    
-    /** -------------------- 책 캐러셀 JS-------------------- */
-    // let currentIndex = {
-    //     'sec_today_sug': 0,
-    //     'sec_pub_rec': 0
-    //     // 추가하고 싶은 sec_이름 추가하면 됨
-    // };
-    
-    // function updateCarousel(containerId) {
-    //     const container = document.getElementById(containerId);
-    //     const index = currentIndex[containerId];
-    //     container.style.transform = `translateX(-${index * 25}%)`; // 25%씩 이동
-    // }
-    
-    // function showNextBook(containerId) {
-    //     const books = document.querySelectorAll(`#${containerId} > div`);
-        
-    //     if (currentIndex[containerId] < books.length - 3) { // 마지막에서 3번째 요소까지만 이동 가능
-    //         currentIndex[containerId]++;
-    //         updateCarousel(containerId);
-    //     }
-    // }
-    
-    // function showPrevBook(containerId) {
-    //     if (currentIndex[containerId] > 0) {
-    //         currentIndex[containerId]--;
-    //         updateCarousel(containerId);
-    //     }
-    // }
-    
-    // document.querySelectorAll('#prevButton').forEach(button => {
-    //     button.addEventListener('click', () => showPrevBook(button.dataset.carousel));
-    // });
-    
-    // document.querySelectorAll('#nextButton').forEach(button => {
-    //     button.addEventListener('click', () => showNextBook(button.dataset.carousel));
-    // });
     
 });
